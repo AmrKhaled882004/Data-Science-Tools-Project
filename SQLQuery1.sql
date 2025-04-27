5ce1822a43fd4cc79824104c99343268424c73c8
AmrKhaled882004 <amro83387@gmail.com>
2025-04-27 19:16:46 +0300

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,29 @@
+
+USE DS_Tools_Project;
+
+
+CREATE TABLE Books (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Title NVARCHAR(255) NOT NULL,
+	Price DECIMAL(10, 2),
+    Rating INT,
+    Availability INT,
+    UPC NVARCHAR(255),
+    ProductType NVARCHAR(255),
+    PriceExclTax DECIMAL(10, 2),
+    PriceInclTax DECIMAL(10, 2),
+    Tax DECIMAL(10, 2),
+    NumReviews INT,
+    Description NVARCHAR(MAX)
+	 
+  
+);
+
+
+
+SELECT * FROM Books;
+
+
+
+
+
